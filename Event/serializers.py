3af5e09f348a263dfcbc9a9c38949b88736f7c66
IlliaPetukhov19 <illia@fillyou.in>
2025-07-02 16:38:15 +0200

    created EventSerializerPost and Assign current user as organizer in EventViewSet.perform_create

--- Event/serializers.py
+++ Event/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from .models import Event
 
+
+
+
 class EventSerializerGet(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ["title", "description", "date", "location_city", "organizer", "age_limit"]
+
+
+class EventSerializerPost(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ["title", "description", "date", "location_city", "age_limit"]
--- Event/views.py
+++ Event/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Event
-from .serializers import EventSerializerGet
+from .serializers import EventSerializerGet, EventSerializerPost
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     def get_serializer_class(self):
         if self.action == "list" or self.action == "retrieve":
             return EventSerializerGet
-        return EventSerializerGet
+        return EventSerializerPost
+    
+    def perform_create(self, serializer):
+        serializer.save(organizer=self.request.user)
