c48462009744cbad541694573ec218856f41cab5
IlliaPetukhov19 <illia@fillyou.in>
2025-07-02 18:39:02 +0200

    added JWT Authentication

--- EventManager/urls.py
+++ EventManager/urls.py
@@ -20,6 +20,7 @@ from User.views import UserViewSet
 from rest_framework import routers
 from django.urls import path
 from django.urls import include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = routers.DefaultRouter()
 router.register("events", EventViewSet, basename="events")
@@ -29,4 +30,6 @@ router.register("user", UserViewSet, basename="user")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
