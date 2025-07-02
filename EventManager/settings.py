8ed2c8e8940ada9d62c05c9b4b444f67fae75778
IlliaPetukhov19 <illia@fillyou.in>
2025-07-02 22:47:26 +0200

    added gmail system configuration to settings.oy

--- EventManager/settings.py
+++ EventManager/settings.py
@@ -95,6 +95,15 @@ REST_FRAMEWORK = {
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
