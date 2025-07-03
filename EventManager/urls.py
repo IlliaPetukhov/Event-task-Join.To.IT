db7368d4baf31df36df630a5fd1c1efa8a9ab67d
IlliaPetukhov19 <illia@fillyou.in>
2025-07-03 10:18:53 +0200

    feat: integrate Swagger UI for API documentation

--- EventManager/urls.py
+++ EventManager/urls.py
@@ -21,6 +21,19 @@ from rest_framework import routers
 from django.urls import path
 from django.urls import include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Event API",
+      default_version='v1',
+      description="Документація API для івент-платформи від Ілюшки)",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 router = routers.DefaultRouter()
 router.register("events", EventViewSet, basename="events")
@@ -32,4 +45,6 @@ urlpatterns = [
     path("", include(router.urls)),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
