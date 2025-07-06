0feaa7b512c7f43d15d3bdb101a2a897043a5c25
IlliaPetukhov19 <illia@fillyou.in>
2025-07-06 17:43:48 +0200

    created new permission ReadOnly

--- Event/permissions.py
+++ Event/permissions.py
@@ -1,8 +1,15 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsOwnderOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
+    
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS or request.user.is_superuser:
             return True
         return obj.organizer == request.user
+        
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
         
