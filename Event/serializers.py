ad14b5924ecfea68db23f384c938fc5a42c79672
IlliaPetukhov19 <illia@fillyou.in>
2025-07-06 17:45:14 +0200

    deleted EventSerializerUpdate to use one universal serializer EventSerializerPostOrUpdateOrGetForOrganizerSerializer

--- Event/serializers.py
+++ Event/serializers.py
@@ -1,23 +1,24 @@
 from rest_framework import serializers
 from .models import Event
+from django.contrib.auth.models import User
 
 
+class OrganizerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "email"]
 
 
 class EventSerializerGet(serializers.ModelSerializer):
+    organizer = OrganizerSerializer()
     class Meta:
         model = Event
         fields = ["id", "title", "description", "date", "location_city", "organizer", "age_limit"]
 
 
 
-class EventSerializerUpdate(serializers.ModelSerializer):
+class EventSerializerPostOrUpdateOrGetForOrganizerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ["title", "description", "date", "location_city", "age_limit"]
 
-
-class EventSerializerPost(serializers.ModelSerializer):
-    class Meta:
-        model = Event
-        fields = ["title", "description", "date", "location_city", "age_limit"]
