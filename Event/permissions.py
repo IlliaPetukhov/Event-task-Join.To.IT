c9b0de2da9dd46acb359f685217f710e71df4ec0
IlliaPetukhov19 <illia@fillyou.in>
2025-07-06 17:55:23 +0200

    changed permissions.py

--- Event/permissions.py
+++ Event/permissions.py
@@ -1,8 +1,6 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsOwnderOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        return request.method in SAFE_METHODS
     
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS or request.user.is_superuser:
